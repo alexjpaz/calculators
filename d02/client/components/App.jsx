434ec13a1845837261886c26d9305f15b4fc9d8a
Alexander Paz <alexjpaz@gmail.com>
2017-05-15 08:34:56 -0400

    Added chart and url bookmarking

--- d02/client/components/App.jsx
+++ d02/client/components/App.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import D02Calculator from '../services/D02Calculator';
+import Url from '../services/Url';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {vars:{}};
 
-    this.vars = [
-      "Co",
-      "Hgb",
-      "SaO2",
-      "PaO2",
-      "SvO2",
-      "PvO2",
-      "DesiredDO2VO2ratio"
-    ]
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.d02Calculator = new D02Calculator();
+
+    this.initVars();
   }
 
   handleSubmit(event) {
@@ -32,25 +25,78 @@ export default class App extends React.Component {
     this.state.result = this.d02Calculator.calculate(this.state.vars);
 
     this.setState(this.state);
+
+    this.updateChart();
+  }
+
+  initVars() {
+    this.state = {vars:{}};
+
+    this.vars = [
+      "Co",
+      "Hgb",
+      "SaO2",
+      "PaO2",
+      "SvO2",
+      "PvO2",
+      "DesiredDO2VO2ratio"
+    ];
+
+    this.vars.forEach((v) => {
+      this.state.vars[v] = +Url.getParameterByName(v);
+    });
+
+    this.state.result = this.d02Calculator.calculate(this.state.vars);
+
+    this.updateChart();
+  }
+
+  updateChart() {
+    let dataArray = [];
+    const start = 1
+    const end = 6;
+    const increment = 0.5;
+
+    const VO2 = this.state.result.VO2;
+    const Hgb = this.state.vars.Hgb;
+    const Co = this.state.vars.Co;
+    const SaO2 = this.state.vars.SaO2;
+
+    dataArray.push(['DO2/VO2 Ratio', 'HgB', 'HgB',  { role: 'annotation' }])
+
+    for(let i=start;i<=end;i+=increment) {
+      let x = i;
+      let y = (((((VO2*x)/10)/Co)/(SaO2/100))/1.34)
+
+      let area = undefined;
+
+      if(i > 2 && i < 5) {
+        area = y;
+      }
+
+      dataArray.push([i, y, area, area]);
+    }
+
+
+    window.drawChart(dataArray);
   }
 
   render() {
     return (
-     <div className='container'>
-        <h2>DO2/VO2</h2>
-        <h3><pre>{ JSON.stringify(this.state, null, 4) }</pre></h3>
+      <div>
         <div className='result'>
           <form>
             { this.vars.map((v) => {
               return <div className='form-group'>
                 <label>
                   {v}
-                  <input type="text" name={v} value={this.state.vars[v]} onChange={this.handleChange} />
+                  <input type="text" name={v} value={this.state.vars[v]} onChange={this.handleChange} className='form-control'/>
                 </label>
               </div>
             }) }
           </form>
         </div>
+        <h3><pre>{ JSON.stringify(this.state.result, null, 4) }</pre></h3>
       </div>
     );
   }
--- d02/client/index.js
+++ d02/client/index.js
@@ -2,4 +2,61 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('container'));
+function Bootstrap() {
+  const steps = [];
+
+  steps.push(new Promise((resolve, reject) => {
+    window.google.charts.load('current', {'packages':['corechart']});
+    window.google.charts.setOnLoadCallback(resolve);
+  }))
+
+  return Promise.all(steps);
+}
+
+
+Bootstrap().then(() => {
+  ReactDOM.render(<App />, document.getElementById('container'));
+});
+
+
+window.drawChart = function(dataArray) {
+  if ((typeof google === 'undefined') || (typeof google.visualization === 'undefined')) {
+    return false;
+  }
+
+
+  var data = google.visualization.arrayToDataTable(dataArray);
+
+  var options = {
+    chartArea:{left:20,top:10,width:"100%",height:"80%"},
+    title: 'Hgb Needed for DO2/VO2',
+    legend: { position: 'bottom' },
+    series: {
+      0: {
+        pointSize: 6,
+        lineSize: 2,
+        areaOpacity: 0,
+        label: "value",
+        showTextEvery: 1
+      },
+      1: {
+        pointSize: 6,
+        type: 'area',
+        "annotations": {
+					"highContrast": false,
+					"textStyle": {
+						"color": "#262626",
+						"fontSize": 10,
+						"bold": true
+					}
+				},
+      }
+    },
+  };
+
+  var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
+
+  chart.draw(data, options);
+}
+
+
